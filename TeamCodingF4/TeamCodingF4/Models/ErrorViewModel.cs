95089041fe864706cee7ac7e2306f8f701ce7b3b
MingN <3591558@gmail.com>
2023-01-04 17:37:22 +0800

    Init 1st project files

---
+++ TeamCodingF4/TeamCodingF4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeamCodingF4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
