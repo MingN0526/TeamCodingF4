9bfd3126e89cc91b672ed327f513e47cf5c3b985
Ken <a790712a@gmail.com>
2023-01-14 14:04:11 +0800

    230114_set bootstrap

---
+++ TeamCodingF4/TeamCodingF4/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamCodingF4.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
