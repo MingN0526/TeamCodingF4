d4b4b1bb3885698ecdcb07edeafdfe05fc8b1e17
JackyWang <jackymamawang@gmail.com>
2023-01-18 11:28:44 +0800

    1120118_artical bootstrap

---
+++ TeamCodingF4/TeamCodingF4/Controllers/ArticalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamCodingF4.Controllers
+{
+    public class ArticalController : Controller
+    {
+        public IActionResult ArticalIndex()
+        {
+            return View();
+        }
+    }
+}
