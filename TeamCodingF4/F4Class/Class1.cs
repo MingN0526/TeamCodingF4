6b6b6fe3a8e9d96c408d0a7516d69d105ca710a0
Ken <a790712a@gmail.com>
2023-01-18 16:46:13 +0800

    1120118_建立F4Class for 類別圖

---
+++ TeamCodingF4/F4Class/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace F4Class
+{
+    public class Class1
+    {
+        public TeamCodingF4.Models.MemberModel MemberModel
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
