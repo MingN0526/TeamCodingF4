b4b196d5fdc79ff18965ee25e8e3e62e0a0c2640
Ken <a790712a@gmail.com>
2023-01-29 18:31:14 +0800

    1020129_編寫登入與註冊功能

--- TeamCodingF4/TeamCodingF4/Models/Account/LoginModel.cs
+++ TeamCodingF4/TeamCodingF4/Models/Account/LoginModel.cs
@@ -1,9 +1,17 @@
-﻿namespace TeamCodingF4.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamCodingF4.Models.Account
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "請輸入信箱")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "請輸入密碼")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
 
     }
--- TeamCodingF4/TeamCodingF4/Models/Account/RegisterModel.cs
+++ TeamCodingF4/TeamCodingF4/Models/Account/RegisterModel.cs
@@ -1,12 +1,24 @@
-﻿namespace TeamCodingF4.Models.Account
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamCodingF4.Models.Account
 {
     public class RegisterModel
     {
-
+        [Required(ErrorMessage = "請輸入使用者名稱")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "請輸入信箱")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "請設置密碼")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        public string PasswordConfirmed { get; set; }
+
+        [Compare("Password", ErrorMessage = "兩次密碼輸入不一致!")]
+        [DataType(DataType.Password)]
+        public string ConfirmePassword { get; set; }
 
     }
 }
