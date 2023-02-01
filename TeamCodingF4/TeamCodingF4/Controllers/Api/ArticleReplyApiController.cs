30b56afd4009cac1d9f2b9c769c04522f893cfc0
JackyWang <jackymamawang@gmail.com>
2023-02-01 18:40:25 +0800

    1120201_新增ArticleReply資料表 新增Article文章4筆 新增ArticleReply文章5筆

---
+++ TeamCodingF4/TeamCodingF4/Controllers/Api/ArticleReplyApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeamCodingF4.Data;
+using TeamCodingF4.Models.ApiModel;
+
+namespace TeamCodingF4.Controllers.Api
+{
+    [Route("api/ArticleReply/[action]")]
+    [ApiController]
+    public class ArticleReplyApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public ArticleReplyApiController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        public List<ArticleReplyModel> GetReplyContent()
+        {
+            return _db.ArticlesReply.Select(x => new ArticleReplyModel
+            {
+                ReplyContent=x.ReplyContent,
+                ReplyDate=x.ReplyDate,
+                ReplyPublisher=x.ReplyPublisher
+            }).ToList();
+        }
+    }
+}
--- TeamCodingF4/TeamCodingF4/Data/ApplicationDbContext.cs
+++ TeamCodingF4/TeamCodingF4/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace TeamCodingF4.Data
         {
         }
         public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticleReply> ArticlesReply { get; set; }
         public DbSet<MemberModel> Members { get; set; }
         public DbSet<MemberLike> MemberLikes { get; set; }
 
@@ -38,7 +39,80 @@ namespace TeamCodingF4.Data
                 ArticleContent = "心得分享!",
                 ArticleTitle = "範例"
             });
+            modelBuilder.Entity<Article>().HasData(new Article
+            {
+                ArticleId = 2,
+                ArticleDate = "2022/01/10",
+                ArticlePublisher = "Jacky",
+                ArticleCategory = "發文解惑",
+                ArticleContent = "發文解惑~",
+                ArticleTitle = "範例"
+            });
+            modelBuilder.Entity<Article>().HasData(new Article
+            {
+                ArticleId = 3,
+                ArticleDate = "2022/01/10",
+                ArticlePublisher = "Jacky",
+                ArticleCategory = "閒聊專區",
+                ArticleContent = "閒聊專區~",
+                ArticleTitle = "範例"
+            });
+            modelBuilder.Entity<Article>().HasData(new Article
+            {
+                ArticleId = 4,
+                ArticleDate = "2022/01/10",
+                ArticlePublisher = "Jacky",
+                ArticleCategory = "抱怨專區",
+                ArticleContent = "抱怨專區~",
+                ArticleTitle = "範例"
+            });
+            modelBuilder.Entity<Article>().HasData(new Article
+            {
+                ArticleId = 5,
+                ArticleDate = "2022/01/10",
+                ArticlePublisher = "Jacky",
+                ArticleCategory = "找尋室友",
+                ArticleContent = "找尋室友~",
+                ArticleTitle = "範例"
+            });
+            modelBuilder.Entity<ArticleReply>().HasData(new ArticleReply
+            {
+                ArticleReplyId = 1,
+                ReplyDate = "2022/01/10",
+                ReplyPublisher = "Sam",
+                ReplyContent = "終於快寫好心得分享了~"
+            });
+            modelBuilder.Entity<ArticleReply>().HasData(new ArticleReply
+            {
+                ArticleReplyId = 2,
+                ReplyDate = "2022/01/10",
+                ReplyPublisher = "Sam",
+                ReplyContent = "終於快寫好發文解惑了~"
+            });
+            modelBuilder.Entity<ArticleReply>().HasData(new ArticleReply
+            {
+                ArticleReplyId = 3,
+                ReplyDate = "2022/01/10",
+                ReplyPublisher = "Sam",
+                ReplyContent = "終於快寫好閒聊專區了~"
+            });
+            modelBuilder.Entity<ArticleReply>().HasData(new ArticleReply
+            {
+                ArticleReplyId = 4,
+                ReplyDate = "2022/01/10",
+                ReplyPublisher = "Sam",
+                ReplyContent = "終於快寫好抱怨專區了~"
+            });
+            modelBuilder.Entity<ArticleReply>().HasData(new ArticleReply
+            {
+                ArticleReplyId = 5,
+                ReplyDate = "2022/01/10",
+                ReplyPublisher = "Sam",
+                ReplyContent = "終於快寫好找尋室友了~"
+            });
         }
+
+        
 
 
     }
