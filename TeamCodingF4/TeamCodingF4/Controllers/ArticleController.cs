295641591396d2a2e8e31743b8bfdffed66289eb
JackyWang <jackymamawang@gmail.com>
2023-02-01 17:38:18 +0800

    1120201_刪除Article文章分類的View和Controller Action 更改Layout的討論區改成討論區首頁

--- TeamCodingF4/TeamCodingF4/Controllers/ArticleController.cs
+++ TeamCodingF4/TeamCodingF4/Controllers/ArticleController.cs
@@ -8,26 +8,5 @@ namespace TeamCodingF4.Controllers
         {
             return View();
         }
-
-        public IActionResult Thinking()
-        {
-            return View();
-        }
-        public IActionResult Question()
-        {
-            return View();
-        }
-        public IActionResult Chat()
-        {
-            return View();
-        }
-        public IActionResult Complain()
-        {
-            return View();
-        }
-        public IActionResult Roommate()
-        {
-            return View();
-        }
     }
 }
--- TeamCodingF4/TeamCodingF4/Models/Article.cs
+++ TeamCodingF4/TeamCodingF4/Models/Article.cs
@@ -6,17 +6,35 @@ namespace TeamCodingF4.Models
     [Table("Articles")]
     public class Article
     {
+        /// <summary>
+        /// 文章ID
+        /// </summary>
         [Key]
         public int ArticleId { get; set; }
+        /// <summary>
+        /// 文章日期
+        /// </summary>
         [StringLength(64)]
         public string ArticleDate { get; set; }
+        /// <summary>
+        /// 文章發佈者
+        /// </summary>
         [StringLength(64)]
         public string ArticlePublisher { get; set; }
+        /// <summary>
+        /// 文章分類
+        /// </summary>
         [StringLength(64)]
         public string ArticleCategory { get; set; }
+        /// <summary>
+        /// 文章內容
+        /// </summary>
         [MaxLength(4000)]
         [Column(TypeName ="nvarchar")]
         public string ArticleContent { get; set; }
+        /// <summary>
+        /// 文章標題
+        /// </summary>
         [MaxLength(60)]
         [Column(TypeName = "nvarchar")]
         public string ArticleTitle { get; set; }
