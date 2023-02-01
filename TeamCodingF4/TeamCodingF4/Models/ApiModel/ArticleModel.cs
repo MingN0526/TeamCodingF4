975ad619b8a2588c8918179496555563c043b47b
JackyWang <jackymamawang@gmail.com>
2023-02-02 02:17:29 +0800

    1120202_根據文章分類篩選文章

--- TeamCodingF4/TeamCodingF4/Models/ApiModel/ArticleModel.cs
+++ TeamCodingF4/TeamCodingF4/Models/ApiModel/ArticleModel.cs
@@ -2,9 +2,11 @@
 {
     public class ArticleModel
     {
+        public int ArticleId { get; set; }
         public string ArticleDate { get; set; }
         public string ArticlePublisher { get; set; }
         public string ArticleContent { get; set; }
         public string ArticleTitle { get; set; }
+        public string ArticleCategory { get; set; }
     }
 }
