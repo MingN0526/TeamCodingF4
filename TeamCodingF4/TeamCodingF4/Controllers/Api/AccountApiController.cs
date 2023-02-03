da5844367fca7894ba20307eabca392defc53d18
Ken <a790712a@gmail.com>
2023-02-03 18:06:16 +0800

    1020203_Register改Vue

---
+++ TeamCodingF4/TeamCodingF4/Controllers/Api/AccountApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeamCodingF4.Data;
+using TeamCodingF4.Models.ApiModel;
+
+namespace TeamCodingF4.Controllers.Api
+{
+    [Route("api/account/[action]")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AccountApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public List<AccountModel> PostToRegister()
+        {
+            return _context.Members.Select(x => new AccountModel
+            {
+                Name = x.Name,
+                Email = x.Email,
+                Password = x.Password,
+            }).ToList();
+        }
+    }
+}
---
+++ TeamCodingF4/TeamCodingF4/Models/ApiModel/AccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamCodingF4.Models.ApiModel
+{
+    public class AccountModel
+    {
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "兩次密碼輸入不一致!")]
+        [DataType(DataType.Password)]
+        public string confirmPwd { get; set; }
+
+    }
+}
