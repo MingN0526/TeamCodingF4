f339287936b5ec0b7f96b477d300718c7280c538
Ken <a790712a@gmail.com>
2023-02-04 16:38:35 +0800

    1120204_更新部分註冊功能(驗證、密碼鹽)

--- TeamCodingF4/TeamCodingF4/Controllers/Api/AccountApiController.cs
+++ TeamCodingF4/TeamCodingF4/Controllers/Api/AccountApiController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using TeamCodingF4.Data;
 using TeamCodingF4.Data.Entity;
 using TeamCodingF4.Models.Account;
 using TeamCodingF4.Models.ApiModel;
 using TeamCodingF4.Models.Common;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+using System.Reflection.Metadata.Ecma335;
 
 namespace TeamCodingF4.Controllers.Api
 {
@@ -21,18 +25,24 @@ namespace TeamCodingF4.Controllers.Api
         public ResponseModel<PostToRegisterResponseModel> PostToRegister([FromBody]PostToRegisterRequestModel model)
         {
             var result = new ResponseModel<PostToRegisterResponseModel>(); ;
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid || _context.Members.Any(m => m.Email == model.Email))
             {
                 result.IsOk = false;
                 return result;
             }
             else
             {
+                string salt = Guid.NewGuid().ToString();
+                byte[] addSalt = Encoding.UTF8.GetBytes(model.Password + salt);
+                byte[] hashByte = new SHA256Managed().ComputeHash(addSalt);
+                string hashStr = Convert.ToBase64String(hashByte);
+
                 var _register = new Member
                 {
                     Name = model.Name,
                     Email = model.Email,
-                    Password = model.Password,
+                    Password = hashStr,
+                    PasswordHash = salt,
                     Account = model.Email,
                 };
 
