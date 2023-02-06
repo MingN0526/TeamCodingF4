063af7971e5c3f97a1408612375c67aa5fd94bed
JackyWang <jackymamawang@gmail.com>
2023-02-06 20:56:30 +0800

    1120206_Delete VUE未完成

--- TeamCodingF4/TeamCodingF4/Controllers/Api/ArticleApiController.cs
+++ TeamCodingF4/TeamCodingF4/Controllers/Api/ArticleApiController.cs
@@ -64,66 +64,68 @@ namespace TeamCodingF4.Controllers.Api
             };
         }
 
-        public async Task<Articles> GetId(int id)
+        //public async Task<Articles> GetId(int id)
+        //{
+        //    var articleId = _db.Articles.FirstOrDefault(x => x.ArticleId == id);
+        //    if (articleId == null)
+        //    {
+        //        return null;
+        //    }
+        //    return articleId;
+        //}
+
+
+
+        [HttpPost("{id}")]
+        //[ValidateAntiForgeryToken]
+        public async Task<ApiResultModel> DeleteArticle(ArticleDeleteModel id)
         {
-            var articleId = _db.Articles.FirstOrDefault(x => x.ArticleId == id);
-            if (articleId == null)
-            {
-                return null;
-            }
-            return articleId;
-        }
-
-
-
-        [HttpPost("{id}"), ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<string> DeleteArticle(int id)
-        {
-            Articles? articles = await _db.Articles.FindAsync(id);
-            if (articles == null)
-            {
-                return "找不到欲刪除的紀錄!";
-            }
-
+            Articles articles = await _db.Articles.FindAsync(id);
             _db.Articles.Remove(articles);
-            await _db.SaveChangesAsync();
-
-            return "刪除成功!";
-
+            await _db.SaveChangesAsync();            
+                return new ApiResultModel
+                {
+                    Status = true,
+                    Message = "刪除成功"
+                };
+                return new ApiResultModel
+                {
+                    Status = false,
+                    Message = "刪除失敗"
+                };                                 
         }
 
         
-        [HttpPut("{id}")]
-        [ValidateAntiForgeryToken]
-        public async Task<string> PutArticle(int id, ArticleModel articleModel)
-        {
-            if (id != articleModel.Id)
-            {
-                return null;
-            }
-            Articles? articles = await _db.Articles.FindAsync(articleModel.Id);
-            articles.Content = articleModel.Content;
-            articles.Title = articleModel.Title;
-            _db.Entry(articles).State = EntityState.Modified;
+        //[HttpPut("{id}")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<string> PutArticle(int id, ArticleModel articleModel)
+        //{
+        //    if (id != articleModel.Id)
+        //    {
+        //        return null;
+        //    }
+        //    Articles? articles = await _db.Articles.FindAsync(articleModel.Id);
+        //    articles.Content = articleModel.Content;
+        //    articles.Title = articleModel.Title;
+        //    _db.Entry(articles).State = EntityState.Modified;
 
-            try
-            {
-                await _db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ArticleExists(id))
-                {
-                    return "找不到欲修改的紀錄!";
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return "修改成功!";
-        }
+        //    try
+        //    {
+        //        await _db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ArticleExists(id))
+        //        {
+        //            return "找不到欲修改的紀錄!";
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //    return "修改成功!";
+        //}
 
        
  
--- TeamCodingF4/TeamCodingF4/Models/ApiModel/ArticleDeleteModel.cs
+++ TeamCodingF4/TeamCodingF4/Models/ApiModel/ArticleDeleteModel.cs
@@ -1,7 +1,21 @@
-﻿namespace TeamCodingF4.Models.ApiModel
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeamCodingF4.Models.ApiModel
 {
     public class ArticleDeleteModel
     {
-        public int Id { get; set; }
+        public int ArticleId { get; set; }
+     
+        public DateTime Date { get; set; }
+    
+        public int PublisherId { get; set; }
+       
+        public string Category { get; set; }
+      
+        public string Content { get; set; }
+     
+        public string Title { get; set; }
+
+        public int ViewCount { get; set; }
     }
 }
