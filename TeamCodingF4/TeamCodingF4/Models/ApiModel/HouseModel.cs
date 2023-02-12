d15347d8eb8e8c852ad7eaedcf61cb33aaf07071
JackyWang <jackymamawang@gmail.com>
2023-02-13 03:40:52 +0800

    1120213_弄一部分物件瀏覽 物件資料版型

---
+++ TeamCodingF4/TeamCodingF4/Models/ApiModel/HouseModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TeamCodingF4.Data.Entity;
+
+namespace TeamCodingF4.Models.ApiModel
+{
+    public class HouseModel
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+  
+        public int RoomTypeId { get; set; }
+
+        public int Room { get; set; }
+        public int hall { get; set; }
+        public int bathroom { get; set; }
+
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public string Floor { get; set; }
+        public decimal Price { get; set; }
+        public decimal Miscellaneous { get; set; }
+        public float Meters { get; set; }
+        public int Car { get; set; }
+        public int Motorcycle { get; set; }
+        public int Lease { get; set; }
+        public string EstateVideoPath { get; set; }
+        public string? message { get; set; }
+
+        public virtual ICollection<Equipment> Equipment { get; set; }
+        public virtual ICollection<Condition> Conditions { get; set; }
+        public virtual RoomType RoomType { get; set; }
+        public virtual ICollection<EstateImage> EstateImage { get; set; }
+    }
+}
