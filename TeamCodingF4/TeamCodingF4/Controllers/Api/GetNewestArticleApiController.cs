96c8e41246558e4d40145142d63858ffce02e0bf
DESKTOP-3F7UUJR\RoyYu <st5727997@hotmail.com>
2023-02-16 04:53:47 +0800

    trying to add an api to fetch newest article

---
+++ TeamCodingF4/TeamCodingF4/Controllers/Api/GetNewestArticleApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeamCodingF4.Data;
+using TeamCodingF4.Models.ApiModel;
+
+namespace TeamCodingF4.Controllers.Api
+{
+    [Route("api/GetNewestArticle/[action]")]
+    [ApiController]
+    public class GetNewestArticleApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public GetNewestArticleApiController(ApplicationDbContext data)
+        {
+            _db = data;
+        }
+        //public List<ArticleModel> GetAll()
+        //{
+            
+        //    return _db.Articles.Select(x=>new ArticleModel
+        //    {
+        //       NewestArticleTitle = x.Title
+        //    }).ToList();
+        //}
+
+    }
+}
